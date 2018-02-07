f0c92febc05326cdfa44458ba09c6f7442c01c50
Nils Hartmann <nils@nilshartmann.net>
2018-02-07 19:19:30 +0100

    more tests

--- src/HelloMessage.js
+++ src/HelloMessage.js
@@ -6,19 +6,23 @@ export default class HelloMessage extends React.Component {
     console.log("RENDERING HelloMessage");
     return (
       <div>
-        <input onChange={event => this.props.onUpdateValue(event.target.value)} value={this.props.state.value} />
+        {/* <input onChange={event => this.props.onUpdateValue(event.target.value)} value={this.props.state.value} /> */}
+        <button onClick={this.props.onClickRow}>Click Row (React)</button>{" "}
+        <button onClick={this.props.onUnclickRow}>Unlick Row (React)</button>
         <Table onUpdateChecked={this.props.onUpdateChecked} checkedSet={this.props.state.checked} />
       </div>
     );
   }
 }
 
+const ROWS = 20000;
+
 class Table extends React.PureComponent {
   render() {
     return (
       <table>
         <tbody>
-          {_.range(0, 1000).map(idx => (
+          {_.range(0, 10000).map(idx => (
             <TableRow
               id={idx}
               key={idx}
@@ -39,24 +43,52 @@ class TableRow extends React.Component {
     return x;
   }
 
+  componentDidMount() {
+    this.log = true;
+  }
+
   toggle = e => {
-    console.log(e);
+    const newState = e.target.checked;
+    for (let i = 0; i < 10; i++) {
+      const cellId = `cell_${e.target.dataset.rowid}_${i}`;
+      document.getElementById(cellId).checked = newState;
+    }
+  };
+
+  registerCheckbox = checkBoxRef => {
+    if (!checkBoxRef) {
+      return; // schade
+    }
+
+    checkBoxRef.addEventListener("change", this.toggle);
   };
 
   render() {
-    console.log("RENDERING TableRow");
     const rowId = this.props.idx;
+    this.log && console.log("Render TableRow ", rowId);
 
     return (
       <tr>
         {_.range(0, 10).map(tdIdx => (
           <td id={tdIdx} key={tdIdx}>
+            {/* Ursprünglicher Ansatz, State in React */}
             <input
               type="checkbox"
               id={`cell_${rowId}_${tdIdx}`}
               checked={this.props.checked ? true : false}
               onChange={this.onUpdateChecked}
             />
+
+            {/* State Management über DOM, statt über React. Hängt nach jedem x-ten Click für einen Augenblick */}
+            {/* <input type="checkbox" id={`cell_${rowId}_${tdIdx}`} data-rowid={rowId} onClick={this.toggle} /> */}
+
+            {/* State Management über DOM auch Event Listener und Handler über nativen DOM. 
+                Selbes Problem: nach jedem x-ten Click hängt die Anwendung
+            */}
+            {/* <input type="checkbox" id={`cell_${rowId}_${tdIdx}`} data-rowid={rowId} ref={this.registerCheckbox} /> */}
+
+            {/* Wie ursprünglich: State über React, Changes aber nur über die Buttons, dh hier kein Listener mehr */}
+            {/* <input type="checkbox" id={`cell_${rowId}_${tdIdx}`} checked={this.props.checked ? true : false} /> */}
           </td>
         ))}
       </tr>
--- src/main.js
+++ src/main.js
@@ -4,32 +4,51 @@ import ReactDOM from "react-dom";
 import HelloMessage from "./HelloMessage";
 
 function renderImpl() {
-	const mountNode = document.getElementById("mount");
-	console.time("render");
-	ReactDOM.render(<HelloMessage onUpdateValue={updateValue} onUpdateChecked={updateChecked} state={state} />, mountNode);
-	console.timeEnd("render");
+  const mountNode = document.getElementById("mount");
+  console.time("render");
+  ReactDOM.render(
+    <HelloMessage onUpdateChecked={updateChecked} state={state} onClickRow={onClickRow} onUnclickRow={onUnclickRow} />,
+    mountNode
+  );
+  console.timeEnd("render");
 }
 
 // const render = _.throttle(renderImpl, 16);
 const render = renderImpl;
 
 let state = {
-	value: "",
-	checked: {}
+  value: "",
+  checked: {}
 };
 
-function updateValue(value) {
-	console.time("updateValue");
-	state = { ...state, value };
-	console.timeEnd("updateValue");
-	render();
+let currentRow = 0;
+
+// function updateValue(value) {
+// 	console.time("updateValue");
+// 	state = { ...state, value };
+// 	console.timeEnd("updateValue");
+// 	render();
+// }
+
+function onClickRow() {
+  updateChecked(currentRow, !state.checked[currentRow]);
+  currentRow++;
+}
+
+function onUnclickRow() {
+  currentRow--;
+  if (currentRow < 0) {
+    currentRow = 0;
+  }
+
+  updateChecked(currentRow, !state.checked[currentRow]);
 }
 
 function updateChecked(idx, checked) {
-	console.time("updateChecked");
-	state = { ...state, checked: { ...state.checked, [idx]: checked } };
-	console.timeEnd("updateChecked");
-	render();
+  console.time("updateChecked");
+  state = { ...state, checked: { ...state.checked, [idx]: checked } };
+  console.timeEnd("updateChecked");
+  render();
 }
 
 render();
