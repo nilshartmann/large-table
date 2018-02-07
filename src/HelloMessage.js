c4360c7fe05ee1bfdf67e21fd1f381735b40d1f7
Nils Hartmann <nils@nilshartmann.net>
2018-02-07 17:25:04 +0100

    add jquery

--- src/HelloMessage.js
+++ src/HelloMessage.js
@@ -2,74 +2,68 @@ import React from "react";
 import * as _ from "lodash";
 
 export default class HelloMessage extends React.Component {
-	render() {
-		console.log("RENDERING HelloMessage");
-		return (
-			<div>
-				<input onChange={event => this.props.onUpdateValue(event.target.value)} value={this.props.state.value} />
-				<Table onUpdateChecked={this.props.onUpdateChecked} checkedSet={this.props.state.checked} />
-			</div>
-		);
-	}
+  render() {
+    console.log("RENDERING HelloMessage");
+    return (
+      <div>
+        <input onChange={event => this.props.onUpdateValue(event.target.value)} value={this.props.state.value} />
+        <Table onUpdateChecked={this.props.onUpdateChecked} checkedSet={this.props.state.checked} />
+      </div>
+    );
+  }
 }
 
 class Table extends React.PureComponent {
-	render() {
-		return (
-			<table>
-				<tbody>
-					{_.range(0, 5000).map(idx => (
-						<TableRow key={idx} idx={idx} onUpdateChecked={this.props.onUpdateChecked} checked={this.props.checkedSet[idx]} />
-					))}
-				</tbody>
-			</table>
-		);
-	}
+  render() {
+    return (
+      <table>
+        <tbody>
+          {_.range(0, 1000).map(idx => (
+            <TableRow
+              id={idx}
+              key={idx}
+              idx={idx}
+              onUpdateChecked={this.props.onUpdateChecked}
+              checked={this.props.checkedSet[idx]}
+            />
+          ))}
+        </tbody>
+      </table>
+    );
+  }
 }
 
 class TableRow extends React.Component {
-	shouldComponentUpdate(nextProps) {
-		return this.props.checked !== nextProps.checked;
-	}
-	render() {
-		console.log("RENDERING TableRow");
-		return (
-			<tr>
-				<td>
-					<input type="checkbox" checked={this.props.checked ? true : false} onChange={this.onUpdateChecked} />
-				</td>
-				<td>
-					<input type="checkbox" checked={this.props.checked ? true : false} onChange={this.onUpdateChecked} />
-				</td>
-				<td>
-					<input type="checkbox" checked={this.props.checked ? true : false} onChange={this.onUpdateChecked} />
-				</td>
-				<td>
-					<input type="checkbox" checked={this.props.checked ? true : false} onChange={this.onUpdateChecked} />
-				</td>
-				<td>
-					<input type="checkbox" checked={this.props.checked ? true : false} onChange={this.onUpdateChecked} />
-				</td>
-				<td>
-					<input type="checkbox" checked={this.props.checked ? true : false} onChange={this.onUpdateChecked} />
-				</td>
-				<td>
-					<input type="checkbox" checked={this.props.checked ? true : false} onChange={this.onUpdateChecked} />
-				</td>
-				<td>
-					<input type="checkbox" checked={this.props.checked ? true : false} onChange={this.onUpdateChecked} />
-				</td>
-				<td>
-					<input type="checkbox" checked={this.props.checked ? true : false} onChange={this.onUpdateChecked} />
-				</td>
-				<td>
-					<input type="checkbox" checked={this.props.checked ? true : false} onChange={this.onUpdateChecked} />
-				</td>
-			</tr>
-		);
-	}
+  shouldComponentUpdate(nextProps) {
+    const x = this.props.checked !== nextProps.checked;
+    return x;
+  }
 
-	onUpdateChecked = event => {
-		this.props.onUpdateChecked(this.props.idx, event.target.checked);
-	};
+  toggle = e => {
+    console.log(e);
+  };
+
+  render() {
+    console.log("RENDERING TableRow");
+    const rowId = this.props.idx;
+
+    return (
+      <tr>
+        {_.range(0, 10).map(tdIdx => (
+          <td id={tdIdx} key={tdIdx}>
+            <input
+              type="checkbox"
+              id={`cell_${rowId}_${tdIdx}`}
+              checked={this.props.checked ? true : false}
+              onChange={this.onUpdateChecked}
+            />
+          </td>
+        ))}
+      </tr>
+    );
+  }
+
+  onUpdateChecked = event => {
+    this.props.onUpdateChecked && this.props.onUpdateChecked(this.props.idx, event.target.checked);
+  };
 }
